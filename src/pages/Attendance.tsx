92821d6fd18af92e351f373f53d9e541cd429d90
Krypton321123 <coolrajsharma28@gmail.com>
2025-10-17 16:21:42 +0530

    minor fix -2

--- src/pages/Attendance.tsx
+++ src/pages/Attendance.tsx
@@ -133,7 +133,7 @@ const Attendance: React.FC = () => {
       </div>
 
       {/* Attendance Grid */}
-      <div className="overflow-x-auto overflow-y-auto border border-blue-500 rounded-md">
+      <div className="overflow-x-auto overflow-y-scroll border border-blue-500 rounded-md max-h-[68vh]">
         <table className="min-w-full">
           <thead>
             <tr className="bg-blue-500 text-white">
